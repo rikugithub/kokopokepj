d45486596ef73f86f2413ac381288f1635074b43
中山早妃 <n.ystn0818@outlook.com>
2019-12-02 12:06:59 +0900

    Initial Commit

---
+++ kokopokePJ/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  kokopokePJ
+//
+//  Created by Saki Nakayama on 2019/12/02.
+//  Copyright © 2019 Saki Nakayama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

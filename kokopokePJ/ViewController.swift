dae55ee8e765fa8e21ac6688f86a0209194278cc
中山早妃 <n.ystn0818@outlook.com>
2019-12-04 16:29:55 +0900

    画面遷移できるようになりました

--- kokopokePJ/ViewController.swift
+++ kokopokePJ/ViewController.swift
@@ -10,11 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var topImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let image = UIImage(named: "whiteLogo")
+        topImage.image = image
+        
+        let _ : Timer = Timer.scheduledTimer(timeInterval:2, target: self, selector: #selector(pageTransition), userInfo: nil, repeats: false)
+        
     }
-
-
+    @objc func pageTransition(timer : Timer) {
+        print("hello!")
+        let storyboard: UIStoryboard = self.storyboard!
+        let top = storyboard.instantiateViewController(identifier: "top")
+        self.present(top,animated: false,completion: nil)
+    }
 }
 
---
+++ kokopokePJ/topViewController.swift
@@ -0,0 +1,20 @@
+//
+//  topViewController.swift
+//  kokopokePJ
+//
+//  Created by Saki Nakayama on 2019/12/04.
+//  Copyright © 2019 Saki Nakayama. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class topViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        
+        
+    }
+    
+}
+  

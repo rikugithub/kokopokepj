69f810acd311d7c54fcad69b24cba480d5184b07
中山早妃 <n.ystn0818@outlook.com>
2019-12-12 10:20:30 +0900

    menuViewのリスト作りました

--- kokopokePJ/menuViewController.swift
+++ kokopokePJ/menuViewController.swift
@@ -9,16 +9,28 @@
 import Foundation
 import UIKit
 
-class menuViewController: UIViewController {
+class menuViewController: UIViewController,UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var backButton: UIImageView!
-    
+    let list = ["訪れた場所","行きたい場所","設定"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         backButton.isUserInteractionEnabled = true
        //backButtonnがタップされたら呼ばれる
         backButton.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(self.backTaped(_:))))
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return list.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        // セルに表示する値を設定する
+        cell.textLabel!.text = list[indexPath.row]
+        return cell
     }
     
     @objc func backTaped(_ sender : UITapGestureRecognizer) {

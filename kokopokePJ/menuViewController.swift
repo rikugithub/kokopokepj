5c81fb5574517dc2e92dd5a9ae670045db174925
中山早妃 <n.ystn0818@outlook.com>
2019-12-12 09:46:50 +0900

    昨日くらいまで戻したぶんです

--- kokopokePJ/menuViewController.swift
+++ kokopokePJ/menuViewController.swift
@@ -2,20 +2,28 @@
 //  menuViewController.swift
 //  kokopokePJ
 //
-//  Created by Saki Nakayama on 2019/12/06.
+//  Created by Saki Nakayama on 2019/12/12.
 //  Copyright © 2019 Saki Nakayama. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
 class menuViewController: UIViewController {
 
+    @IBOutlet weak var backButton: UIImageView!
     
-    var menu :String = ["訪れた場所","行きたい場所","設定"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        backButton.isUserInteractionEnabled = true
+       //backButtonnがタップされたら呼ばれる
+        backButton.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(self.backTaped(_:))))
     }
     
-    
+    @objc func backTaped(_ sender : UITapGestureRecognizer) {
+        let _: UIStoryboard = self.storyboard!
+        let top = storyboard?.instantiateViewController(identifier: "top")
+        self.present(top!,animated: true,completion: nil)
+    }
 }

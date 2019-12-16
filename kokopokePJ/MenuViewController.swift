5fa387b158a84274d768fe1a7873948c8b44777c
Sikajika <gondo.syun0711@gmail.com>
2019-12-16 12:24:52 +0900

    StroyBoardとViewControllerの紐付けを実行(動作確認済み)

--- kokopokePJ/MenuViewController.swift
+++ kokopokePJ/MenuViewController.swift
@@ -16,11 +16,11 @@ class MenuViewController: UIViewController, UITableViewDataSource ,UITableViewDe
     var selectedText : String?
     
     let list = ["訪れた場所","行きたい場所","設定"]
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         backToTop.isUserInteractionEnabled = true
-       //backToTopがタップされたら呼ばれる
+        //backToTopがタップされたら呼ばれる
         backToTop.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(self.backTaped(_:))))
         
         tableView.delegate = self
@@ -44,30 +44,30 @@ class MenuViewController: UIViewController, UITableViewDataSource ,UITableViewDe
     }
     
     //cellが選択された時の処理
-        func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-            
-            let a: Int = indexPath.row
-            
-            switch a {
-            case (0) :
-                //訪れた場所
-                let _: UIStoryboard = self.storyboard!
-                let visited = storyboard?.instantiateViewController(identifier: "visited")
-                self.present(visited!,animated: true,completion: nil)
-            case (1) :
-                //行きたい場所
-                let _: UIStoryboard = self.storyboard!
-                let wantToGo = storyboard?.instantiateViewController(identifier: "wantToGo")
-                self.present(wantToGo!,animated: true,completion: nil)
-            case (2) :
-                //設定
-                let _: UIStoryboard = self.storyboard!
-                let setting = storyboard?.instantiateViewController(identifier: "setting")
-                self.present(setting!,animated: true,completion: nil)
-            default :
-                print("エラー")
-            }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        let a: Int = indexPath.row
+        
+        switch a {
+        case (0) :
+            //訪れた場所
+            let _: UIStoryboard = self.storyboard!
+            let visited = storyboard?.instantiateViewController(identifier: "visited")
+            self.present(visited!,animated: true,completion: nil)
+        case (1) :
+            //行きたい場所
+            let _: UIStoryboard = self.storyboard!
+            let wantToGo = storyboard?.instantiateViewController(identifier: "wantToGo")
+            self.present(wantToGo!,animated: true,completion: nil)
+        case (2) :
+            //設定
+            let _: UIStoryboard = self.storyboard!
+            let setting = storyboard?.instantiateViewController(identifier: "setting")
+            self.present(setting!,animated: true,completion: nil)
+        default :
+            print("エラー")
         }
+    }
     
     
     @objc func backTaped(_ sender : UITapGestureRecognizer) {
--- kokopokePJ/VisitedViewController.swift
+++ kokopokePJ/VisitedViewController.swift
@@ -10,12 +10,43 @@ import Foundation
 import UIKit
 
 //訪れた場所
-class VistedViewController: UIViewController{
+class VistedViewController: UIViewController, UITableViewDataSource ,UITableViewDelegate{
+    
+    var selectedText : String?
+    
+    @IBOutlet weak var tableView:UITableView!
+    
+    let list = ["北海道情報専門学校","ラソラ東札幌店","アークス菊水店","つぼ八東札幌店"]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return list.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        // セルに表示する値を設定する
+        cell.textLabel!.text = list[indexPath.row]
+        return cell
+    }
+    
+    //cellが選択された時の処理
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+  
+        let _: UIStoryboard = self.storyboard!
+        let visited = storyboard?.instantiateViewController(identifier: "visitedDetail")
+        self.present(visited!,animated: true,completion: nil)
+    }
+    
+    
+    
     
     @IBOutlet weak var backToMenuFromVisited: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //tableView.delegate = self
+        //tableView.dataSource = self
         backToMenuFromVisited.isUserInteractionEnabled = true
        //bToMenuFromvisitedがタップされたら呼ばれる
         backToMenuFromVisited.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(self.backTaped(_:))))

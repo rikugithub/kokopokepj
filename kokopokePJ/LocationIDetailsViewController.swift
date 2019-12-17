5aaf90b5c9eb214d30cb02ccfaf8547d4276b1a1
中山早妃 <n.ystn0818@outlook.com>
2019-12-17 14:58:29 +0900

    検索バーから遷移する詳細画面を作成

---
+++ kokopokePJ/LocationIDetailsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LocationInforViewController.swift
+//  kokopokePJ
+//
+//  Created by Saki Nakayama on 2019/12/17.
+//  Copyright © 2019 Saki Nakayama. All rights reserved.
+//
+
+import UIKit
+
+class LocationDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

df65d6b4d85aedd36ab45018500fea636f177c44
Sikajika <gondo.syun0711@gmail.com>
2019-12-21 18:05:00 +0900

    デザイン拡張クラスの追加

---
+++ kokopokePJ/UIView+Border.swift
@@ -0,0 +1,44 @@
+//
+//  UIView+Border.swift
+//  kokopokePJ
+//
+//  Created by しゅん on 2019/12/21.
+//  Copyright © 2019 Saki Nakayama. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+
+    // 枠線の色
+    @IBInspectable var borderColor: UIColor? {
+        get {
+            return layer.borderColor.map { UIColor(cgColor: $0) }
+        }
+        set {
+            layer.borderColor = newValue?.cgColor
+        }
+    }
+
+    // 枠線のWidth
+    @IBInspectable var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+
+    // 角丸設定
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+            layer.masksToBounds = newValue > 0
+        }
+    }
+}

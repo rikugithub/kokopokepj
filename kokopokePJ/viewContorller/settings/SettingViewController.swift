7a6a97809d3c24ae42d28ad2d0fc189d31e79ef5
中山早妃 <n.ystn0818@outlook.com>
2020-01-29 15:56:00 +0900

    設定画面作成（まだ動きません）

--- kokopokePJ/viewContorller/settings/SettingViewController.swift
+++ kokopokePJ/viewContorller/settings/SettingViewController.swift
@@ -9,11 +9,47 @@
 import Foundation
 import UIKit
 
-//訪れた場所
-class SettingViewController: UIViewController{
+//設定
+class SettingTableViewController: UIViewController, UITableViewDataSource{
+
+    
+    @IBOutlet weak var postNameSetting: UITableViewCell!
+    @IBOutlet weak var postNameLabel: UILabel!
+    @IBOutlet weak var shareSwitch: UISwitch!
+    @IBOutlet weak var GPSSetting: UIView!
+    @IBOutlet weak var GPSsettingLabel: UILabel!
+    @IBOutlet weak var searchHistoryClear: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 3
+    }
+    //セルの中身設定
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell : UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel!.text = "aaaa"
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case 0: // 「口コミ投稿情報」のセクション
+            return 2
+        case 1 : // 「GPS」のセクション
+            return 2
+        case 2: //「検索履歴」のセクション
+            return 1
+        default: // ここが実行されることは無いはず
+          return 0
+        }
+    }
+    
+    //cellが選択された時の処理
+     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+     }
     
 }
